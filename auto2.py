80bfec46e6a259d5b41c1c82c5df3e0aade2626c
all4fun123 <59323993+all4fun123@users.noreply.github.com>
2025-08-11 23:58:10 +0700

    Add files via upload

--- auto2.py
+++ auto2.py
@@ -260,5 +260,5 @@ def run_background():
 if __name__ == "__main__":
     t = threading.Thread(target=run_background, daemon=True)
     t.start()
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
